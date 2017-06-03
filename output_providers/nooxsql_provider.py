bcd95e12e27c734932e281878c01de5e02f562f5
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-06-04 00:01:07 +0700

    nooxsqlprovider categorizer & config

--- output_providers/nooxsql_provider.py
+++ output_providers/nooxsql_provider.py
@@ -1,11 +1,12 @@
 import pymysql
 import hashlib
+import re
 from output_providers import BaseProvider
 
 
 class NooxSqlProvider(BaseProvider):
 
-    def __init__(self, config, data=None):
+    def __init__(self, config, noox_config, data=None):
         if not isinstance(config, dict):
             raise TypeError('config parameter is expected to be dict instance')
         if not all(key in ('db_url', 'db_username', 'db_password', 'db_name') for key in config):
@@ -18,6 +19,8 @@ class NooxSqlProvider(BaseProvider):
         else:
             self.config.update({'db_charset': 'latin-1'})
 
+        self.noox_config = noox_config
+
         self.data = data
 
         self._db = pymysql.connect(
@@ -26,6 +29,10 @@ class NooxSqlProvider(BaseProvider):
             self.config['db_password'],
             self.config['db_name'],
             charset='utf8')
+
+        self._source_id = self._get_noox_news_source_id()
+        self._noox_categories = self._get_noox_categories()
+        self._url_regex = re.compile(self.noox_config['url_regex'])
 
     def size(self):
         if self.data is not None:
@@ -43,17 +50,56 @@ class NooxSqlProvider(BaseProvider):
         if len(data) < 1:
             raise ValueError('No data to output!')
 
+        filteredData = [item for item in data if self._get_category_id(item['url']) is not None]
+        if len(filteredData) < 1:
+            raise RuntimeError('Output data is zero after skipping data with no category.')
+
         cursor = self._db.cursor()
         baseSql = 'INSERT INTO `news` (`title`, `source_id`, `cat_id`, `url`, `url_hash`, `author`, `pubtime`, `content`) VALUES {0};'
-        in_p = ', '.join(map(lambda x: "('"+self._db.escape_string(x['title'])+"', 3, 1, '"+self._db.escape_string(x['url'])+"', '"+self._db.escape_string(self._get_md5(x['url']))+"', '"+self._db.escape_string(x['author'])+"', '"+self._db.escape_string(x['pubtime'])+"', '"+self._db.escape_string(x['content'])+"')", data))
+        in_p = ', '.join([item for item in map(self._format_sql, filteredData)])
         sql = baseSql.format(in_p)
         # print(sql)
         cursor.execute(sql)
         self._db.commit()
-        self.lastinsertids = [i for i in range(cursor.lastrowid, cursor.lastrowid + len(data))]
+        self.lastinsertids = [i for i in range(cursor.lastrowid, cursor.lastrowid + len(filteredData))]
         return self.lastinsertids
 
     def _get_md5(self, string: str):
         m = hashlib.md5()
         m.update(string.encode('utf8'))
         return m.hexdigest()
+
+    def _get_noox_categories(self):
+        cursor = self._db.cursor()
+        sql = 'SELECT * FROM `news_categories`'
+        cursor.execute(sql)
+        return dict((name, id) for id, name in cursor.fetchall())
+
+    def _get_category_id(self, url):
+        keyword = self._url_regex.search(url).group(1)
+        if keyword in self.noox_config['categories']:
+            category_name = self.noox_config['categories'][keyword]
+        else:
+            if self.noox_config['allow_default_category']:
+                category_name = self.noox_config['default_category']
+            elif self.noox_config['skip_when_no_category']:
+                print('Skipping: {0} (no category)'.format(url))
+                return None
+            else:
+                raise Exception('Cannot determine category from {0}'.format(url))
+        return self._noox_categories[category_name]
+
+    def _get_noox_news_source_id(self):
+        cursor = self._db.cursor()
+        sql = 'SELECT `id` FROM `news_sources` WHERE `source_name` = {0}'.format("'"+self._db.escape_string(self.noox_config['db_sitename'])+"'")
+        if cursor.execute(sql) == 1:
+            return cursor.fetchone()[0]
+        else:
+            raise Exception('Source id could not be found from database. Check your configuration file.')
+
+    def _format_sql(self, item):
+        category_id = self._get_category_id(item['url'])
+        if category_id is not None:
+            return "('"+self._db.escape_string(item['title'])+"', '"+str(self._source_id)+"', '"+str(category_id)+"', '"+self._db.escape_string(item['url'])+"', '"+self._get_md5(item['url'])+"', '"+self._db.escape_string(item['author'])+"', '"+self._db.escape_string(item['pubtime'])+"', '"+self._db.escape_string(item['content'])+"')" 
+        else:
+            raise Exception('No categories reached _format_sql')
--- tester.py
+++ tester.py
@@ -1,7 +1,5 @@
 import os
 import json
-import re
-import pymysql
 from modules import LinkExtractor, NewsGrabber
 from output_providers import NooxSqlProvider, JsonProvider
 # from dateutil.parser import parser as dateparse
@@ -10,35 +8,19 @@ conf_dir = './config/liputan6.conf.json'
 if os.path.isfile(conf_dir):
     with open(conf_dir) as conf_file:
         config = json.load(conf_file)
-        regex = re.compile(config['url_regex'])
         # print(config)
         a = LinkExtractor(config)
         # print(a.get_urls(1))
         links = a.get_urls(max_link=50)
         grabber = NewsGrabber(config)
-        news = grabber.process(links[:15])
+        news = grabber.process(links[:30])
         print('Scanned '+str(len(news))+' out of '+str(len(links))+' links...')
+
         file = JsonProvider('liputan6.json', True, news)
         print('Writing to file...')
-        # for data in news:
-        #     groups = regex.search(link)
-        #     if groups is None:
-        #         subd = 'ERR'
-        #     else:
-        #         subd = groups.group(1)
-        #     file.write(subd+" : "+link+'\n')
-        # file.write(str(links))
-
         file.save()
-        out = NooxSqlProvider({'db_url': 'localhost', 'db_username': 'root', 'db_password': '', 'db_name': 'nooxdb'}, news)
-        # db = pymysql.connect('localhost', 'root', '', 'nooxdb', charset='utf8')
-        # cursor = db.cursor()
-        # sql = 'INSERT INTO `news` (`title`, `source_id`, `cat_id`, `url`, `author`, `pubtime`, `content`) VALUES {0};'
-        # in_p = ', '.join(map(lambda x: "('"+db.escape_string(x['title'])+"', 3, 1, '"+db.escape_string(x['url'])+"', 'anon', '"+db.escape_string(x['pubtime'])+"', '"+db.escape_string(x['content'])+"')", news))
-        # sql = sql.format(in_p)
-        # # print(sql)
-        # cursor.execute(sql)
-        # db.commit()
-        # ids = [i for i in range(cursor.lastrowid, cursor.lastrowid + len(news))]
+
+        out = NooxSqlProvider({'db_url': 'localhost', 'db_username': 'root', 'db_password': '', 'db_name': 'nooxdbapi'}, config['noox_config'], news)
+        print('Saving to database...')
         ids = out.save()
         print(ids)
