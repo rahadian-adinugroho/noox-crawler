8026c08ebf3b88a4e646f3510c5e65a23d67f2ce
Rahadian Adinugroho <rahadian.adinugroho@live.com>
2017-06-19 22:42:39 +0700

    verbose messages & limit flag

--- modules/link_extractor.py
+++ modules/link_extractor.py
@@ -22,13 +22,11 @@ class LinkExtractor:
 
     _config = {}
 
-    __verboseprint = None
-
     def __init__(self, config, start_url=None, debug=False, verbose=False):
         self._config.update(config)
         self._is_debug = debug
         self._is_verbose = verbose
-        __verboseprint = print if self._is_verbose or self._is_debug else lambda *a, **k: None
+        self.__verboseprint = print if self._is_verbose or self._is_debug else lambda *a, **k: None
         if isinstance(start_url, str):
             self._edges.put(start_url)
 
@@ -56,14 +54,14 @@ class LinkExtractor:
 
         depth = 0
         sleep(0.001)
-        print('Starting url scanning...')
+
         while not self._edges.empty() and (not max_link or len(self._links) < max_link):
             edge = self._edges.get(block=True)
-            __verboseprint('Retrieving data from "'+edge+'"')
+            self.__verboseprint('Retrieving data from "'+edge+'"')
             page = requests.get(edge, headers=self._header)
             # if we got a sitemap index, add these sitemaps to our edge list
             if self._is_sitemap_index(page) and depth == 0:
-                __verboseprint('Got sitemap index...')
+                self.__verboseprint('Got sitemap index...')
                 if 'sitemapindex_regex' in self._config and len(self._config['sitemapindex_regex']) > 0:
                     sitemapIndexRegex = re.compile(self._config['sitemapindex_regex'])
                 else:
@@ -76,7 +74,7 @@ class LinkExtractor:
             else:
                 # if we got a sitemap, add the locs to the our links list
                 if self._is_sitemap(page):
-                    __verboseprint('Got sitemap...')
+                    self.__verboseprint('Got sitemap...')
                     for loc in BeautifulSoup(page.text, 'lxml-xml', parse_only=SoupStrainer('loc')):
                         url = loc.get_text().strip()
                         if regex.match(url) and url not in self._links:
@@ -95,8 +93,6 @@ class LinkExtractor:
                                 sleep(0.001)
             depth += 1
 
-        print('Found '+str(len(self._links))+' links...')
-        print('Operation finished...')
         return self._links
 
     def _is_sitemap_index(self, page):
--- tester.py
+++ tester.py
@@ -12,8 +12,6 @@ o_providers = {
     'NooxDB': NooxSqlProvider
 }
 
-alnum_re = re.compile(r'^[A-Za-z0-9]{3,}$')
-
 
 def check_with_db(urls):
     db = pymysql.connect('localhost', 'root', '', 'nooxdbapi')
@@ -21,18 +19,17 @@ def check_with_db(urls):
     sql = 'SELECT `url` FROM `news` WHERE `url_hash` IN ({0})'
     in_p = ', '.join(map(lambda x: "'" + hashlib.md5(x.encode('utf-8')).hexdigest() + "'", urls))
     sql = sql.format(in_p)
-    print(sql)
-    # print(sql)
     cursor.execute(sql)
     return [row[0] for row in cursor.fetchall()]
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Noox crawler driver.')
-    parser.set_defaults(debug=False, verbose=False)
+    parser.set_defaults(debug=False, verbose=False, limit=500)
     parser.add_argument('--debug', action='store_true', help='exit immediately when error occured')
     parser.add_argument('-v', '--verbose', action='store_true', help='message verbosity')
     parser.add_argument('-o', '--output', action='append', type=str, help='output providers (default = json), can be multiple (-o json -o other)')
+    parser.add_argument('-l', '--limit', type=int, help='limit links to be scanned')
     parser.add_argument('target', help='site to be scanned (enter "all" to scan all sites)')
     return parser.parse_args()
 
@@ -40,14 +37,14 @@ def parse_args():
 def process_output_providers(destinations, config):
     o_destinations = []
     if destinations is None:
-        o_destinations = o_providers['json'](config['sitename'], True)
+        o_destinations.append(o_providers['json'](config['sitename']+'.json', True))
     else:
         for dest in destinations:
             if dest not in o_providers:
                 raise ImportError('Provider for {0} not found.'.format(dest))
 
             if dest == 'json':
-                initialized = o_providers[dest](config['sitename'], True)
+                initialized = o_providers[dest](config['sitename']+'.json', True)
             elif dest == 'NooxDB':
                 initialized = o_providers[dest]({'db_url': 'localhost', 'db_username': 'root', 'db_password': '', 'db_name': 'nooxdbapi'}, config['noox_config'])
 
@@ -57,31 +54,36 @@ def process_output_providers(destinations, config):
 
 def main():
     args = parse_args()
+    alnum_re = re.compile(r'^[A-Za-z0-9]{3,}$')
+    verboseprint = print if args.verbose or args.debug else lambda *a, **k: None
 
     if args.target == 'all':
         pass
     elif alnum_re.match(args.target):
+        print('Scanning site: {0}'.format(args.target.title()))
         conf_dir = './config/{0}.conf.json'.format(args.target)
         if os.path.isfile(conf_dir):
             with open(conf_dir) as conf_file:
                 config = json.load(conf_file)
-                # print(config)
-                print(process_output_providers(args.output, config))
+
+                verboseprint('Starting url scanning...')
                 a = LinkExtractor(config, debug=args.debug, verbose=args.verbose)
-                # print(a.get_urls(1))
-                links = a.get_urls(max_link=100)
+                links = a.get_urls(max_link=args.limit)
+                verboseprint('Found '+str(len(links))+' links...')
+                verboseprint('Link extract finished...')
+
+                verboseprint('Grabbing news data...')
                 grabber = NewsGrabber(config, debug=args.debug, verbose=args.verbose)
-                news = grabber.process(links[:10], url_check_callback=check_with_db)
-                print('Scanned '+str(len(news))+' out of '+str(len(links))+' links...')
+                if args.limit > 0:
+                    news = grabber.process(links[:args.limit], url_check_callback=check_with_db)
+                else:
+                    news = grabber.process(links, url_check_callback=check_with_db)
+                verboseprint('Scanned '+str(len(news))+' out of '+str(len(links))+' links...')
 
-                file = JsonProvider('liputan6.json', True, news)
-                print('Writing to file...')
-                file.save()
-
-                out = NooxSqlProvider({'db_url': 'localhost', 'db_username': 'root', 'db_password': '', 'db_name': 'nooxdbapi'}, config['noox_config'], news)
-                # print('Saving to database...')
-                ids = out.save()
-                print(ids)
+                for output in process_output_providers(args.output, config):
+                    verboseprint('Using output provider: {0}'.format(output.__class__.__name__))
+                    output.save(news)
+                print('Operation finished...')
         else:
             raise OSError(2, 'Configuration file not found', './config/{0}.conf.json'.format(args.target))
 
